c883ea3b7c7890ad7aac749f24efaaddfb3b1347
icanq <icanatawijaya@gmail.com>
2023-09-21 19:18:11 +0700

    feat: serve static files

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
 // import module http dari builtin module node.js
 const http = require('http');
 
+// fs adalah module bawaan dari node.js yang digunakan untuk membaca file, atau menulis file.
+const fs = require('fs');
+
 const server = http.createServer((request, response) => {
-  console.log(request, "<<< request")
-  console.log(response, "<<< response")
+  // kita ingin menangani setiap request yang masuk melalui endpoint atau rute "/" kita akan memberikan response berupa file html, yaitu index.html yang berada pada folder public. 
 
-  // bikin rute untuk url "/" dengan method "GET"
-  if (request.url === "/") {
-    response.write("sesuatu");
-    // kalau tidak memberikan response.end, maka setiap request yang terjadi tidak akan berhenti di server, karena di server tidak memberitahu kapan untuk berhenti. Kalaupun request berhenti, itu karena RTO Request TimeOut
-    // ibaratnya kaya di kacangin
-    response.end()
-  }
+  // ketika server dibuka pada port 3000, setiap kali ada request yang menuju rute "/" maka kita akan mengirimkan file index.html yang berada pada folder public.
 
-  // bikin rute untuk url "/me" dengan method "GET"
-  if (request.url === "/me" && request.method === "GET") {
-    response.end("<h1 style='color: red;'>ini adalah halaman me</h1>")
+  if (request.url === '/' && request.method === "GET") {
+    // const htmlFile = fs.readFileSync('./public/index.html', 'utf-8')
+    // response.end(htmlFile)
+    fs.readFile('./public/index.html', (error, data) => {
+      // apabila terjadi error dalam pembacaan file maka akan kirim error
+      if (error) {
+        response.end('Terjadi kesalahan dalam pembacaan file html')
+      } else {
+        response.end(data)
+      }
+    })
+  } else if (request.url === "/about" && request.method === "GET") {
+    fs.readFile('./public/about.html', (error, data) => {
+      // apabila terjadi error dalam pembacaan file maka akan kirim error
+      if (error) {
+        response.end('Terjadi kesalahan dalam pembacaan file html')
+      } else {
+        response.end(data)
+      }
+    })
+  } else if (request.url === "/teletubbies" && request.method === "GET") {
+    fs.readFile('./public/images/teletubbies.jpeg', (error, data) => {
+      // apabila terjadi error dalam pembacaan file maka akan kirim error
+      if (error) {
+        response.end('Terjadi kesalahan dalam pembacaan file html')
+      } else {
+        response.end(data)
+      }
+    })
+  } else {
+    // menangani rute yang gak ada
+    response.end("404 Not Found - Routes not exists")
   }
-
-  if (request.url === "/nabi" && request.method === "GET") {
-    response.end("Nabi")
-  }
+  
 })
 
-const PORT = 4000;
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server sedang berjalan pada url dan port http://localhost:${PORT}`)
 })
